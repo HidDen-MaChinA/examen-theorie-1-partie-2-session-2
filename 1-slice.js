1c84ad66fc8a2a05c2d0d5152b0f684a3b48ad65
hiDden rafra <98639271+HidDen-MaChinA@users.noreply.github.com>
2022-03-31 16:36:04 +0300

    fix:slice.js

--- 1-slice.js
+++ 1-slice.js
@@ -1,9 +1,16 @@
-function slice(array, start, end) {
-  let slicedArray = [];
-  for (let i = start; i < end; i++) {
-    slicedArray.push(array[i]);
+function slice(array, start, end) {//4
+  let slicedArray = [];//1
+  for (let i = start; i < end; i++) {//6
+    slicedArray.push(array[i]);//5
   }
-  return slicedArray;
+  return slicedArray;//1
 }
 
-module.exports = { slice };
+module.exports = { slice };//1
+
+
+//la complexiter temporelle de l'algorithme 
+//4+1+(6+5)*end+1+1
+//o=1+1(1+1)*n+1+1
+//o=1(1)*n
+//o=n
