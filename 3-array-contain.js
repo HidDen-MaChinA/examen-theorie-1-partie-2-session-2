cba039793364cc036e05a51f1cedc564b5aef047
hiDden rafra <98639271+HidDen-MaChinA@users.noreply.github.com>
2022-03-31 16:57:53 +0300

    fix:3-array-contain.js

--- 3-array-contain.js
+++ 3-array-contain.js
@@ -1,10 +1,17 @@
-function arrayContain(array, number) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === number) {
-      return true;
+function arrayContain(array, number) {//3
+  for (let i = 0; i < array.length; i++) {//6
+    if (array[i] === number) {//5
+      return true;//1
     }
   }
-  return false;
+  return false;//1
 }
 
-module.exports = { arrayContain };
+module.exports = { arrayContain };//1
+
+//complexiter temporelle de l'algorithme 
+//3+(6+5+1+1)*array.length+1
+//o=1+(1+1+1+1)*n+1
+//o=1+(1)n
+//o=n+1
+//o=1
